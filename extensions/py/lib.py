0ebbdb469322e64375f82c200c5b66a24aac498a
Gu Zhengxiong <rectigu@gmail.com>
2016-03-29 20:21:02 +0800

    lib.py: Added.

--- extensions/py/lib.py
+++ extensions/py/lib.py
@@ -1,17 +1,6 @@
-#!/usr/bin/env python3
-
-
 """
 {{ copyright }}
 """
 
 
 from __future__ import division, absolute_import, print_function
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
